de8f3c8183eaa3144113807b8a814eec440caac0
ErlensGadzans <erlens.gadzans@users.noreply.github.com>
2021-02-26 17:51:00 +0200

    Login is not fetching data from be. TS killed me.
    That was a mistake to create tis app with TS

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 // import Output from './components/Output';
 import SearchBar from './components/SearchBar';
+import LoginComponent from './components/Login';
+import { Route, withRouter } from "react-router-dom";
+
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <SearchBar/>
-      
-       
+
+       <Route path="/login" component={LoginComponent} />
+
+       <Route exact path="/" component={SearchBar} />
       </header>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);;
